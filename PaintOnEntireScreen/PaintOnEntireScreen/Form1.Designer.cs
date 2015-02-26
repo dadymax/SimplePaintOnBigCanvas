b1fe13a2548172876d312b64a7a973b711b7f2a5
Max <dady_max@yahoo.co.uk>
2015-02-26 21:53:15 +0300

    Simple paint on big panel

---
+++ PaintOnEntireScreen/PaintOnEntireScreen/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace PaintOnEntireScreen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnl_Draw = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnl_Draw.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnl_Draw
+            // 
+            this.pnl_Draw.BackColor = System.Drawing.Color.White;
+            this.pnl_Draw.Controls.Add(this.label1);
+            this.pnl_Draw.Location = new System.Drawing.Point(0, 0);
+            this.pnl_Draw.Name = "pnl_Draw";
+            this.pnl_Draw.Size = new System.Drawing.Size(5000, 5000);
+            this.pnl_Draw.TabIndex = 0;
+            this.pnl_Draw.MouseEnter += new System.EventHandler(this.pnl_Draw_MouseEnter);
+            this.pnl_Draw.MouseLeave += new System.EventHandler(this.pnl_Draw_MouseLeave);
+            this.pnl_Draw.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnl_Draw_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2771, 913);
+            this.Controls.Add(this.pnl_Draw);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.pnl_Draw.ResumeLayout(false);
+            this.pnl_Draw.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl_Draw;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PaintOnEntireScreen/PaintOnEntireScreen/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PaintOnEntireScreen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            g = pnl_Draw.CreateGraphics();
+            currentColor = colors[1];
+        }
+
+        Graphics g;
+        Color cc;
+        bool stopDrawing = false;
+
+        Dictionary<Keys, int> keys2char = new Dictionary<Keys, int>()
+        {
+            {Keys.D1, 1},
+            {                Keys.D2, 2            },
+            {                Keys.D3, 3            },
+            {                Keys.D4, 4            },
+            {                Keys.D5, 5            },
+            {                Keys.D6, 6            },
+            {                Keys.D7, 7            },
+            {                Keys.D8, 8            },
+            {                Keys.D9, 9            },
+            {                Keys.D0, 0            }
+        };
+
+        public Color currentColor
+        {
+            get { return cc; }
+            set { cc = value; label1.Text = cc.Name; }
+        }
+
+        public Color[] colors = { Color.White, Color.Green, Color.Red, Color.Blue, Color.Violet, Color.Black };
+
+        int? oldX = null;
+        int? oldY = null;
+
+        private void pnl_Draw_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!stopDrawing)
+            {
+                Pen pen = new Pen(currentColor, 10);
+                g.DrawLine(pen, new Point(oldX ?? e.X, oldY ?? e.Y), new Point(e.X, e.Y));
+            }
+            oldX = e.X;
+            oldY = e.Y;
+        }
+
+        private void pnl_Draw_MouseEnter(object sender, EventArgs e)
+        {
+            //label1.Text = "entered";
+        }
+
+        private void pnl_Draw_MouseLeave(object sender, EventArgs e)
+        {
+            //label1.Text = "out";
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode >= Keys.D0 && e.KeyCode <= Keys.D9 && keys2char[e.KeyCode] < colors.Length)
+                currentColor = colors[keys2char[e.KeyCode]];
+            else
+                if (e.KeyCode == Keys.Space)
+                    g.Clear(pnl_Draw.BackColor);
+                else
+                    if (e.KeyCode == Keys.Tab)
+                        stopDrawing = !stopDrawing;
+        }
+    }
+}
